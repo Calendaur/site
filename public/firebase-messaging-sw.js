b66cf6eb0058ba12242cc29284a2179a5dc81faa
Danil Shashkov <shashkovdanil@gmail.com>
2020-03-07 21:57:22 +0500

    Try use push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,12 @@
-import { config } from '../src/components/firebase-provider'
-
 importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-messaging.js')
 
-firebase.initializeApp(config)
+firebase.initializeApp({
+  apiKey: 'AIzaSyBocY4_tAx1Y85w5agrgYK7ptcYxgRWcb8',
+  projectId: 'release-calendar-487f9',
+  messagingSenderId: '564145539525',
+  appId: '1:564145539525:web:a633ca316fe96db04f23ac',
+})
 
 firebase.messaging()
 
