96830c9bde085db52fbff493e4eedf957e2a10a5
Danil Shashkov <shashkovdanil@gmail.com>
2020-03-07 21:48:04 +0500

    Try use push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,19 @@
+import { config } from '../src/components/firebase-provider'
+
 importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-messaging.js')
 
-firebase.initializeApp({
-  messagingSenderId: '564145539525',
-})
+firebase.initializeApp(config)
 
 firebase.messaging()
+
+const messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  const title = 'Hello World'
+  const options = {
+    body: payload.data.status,
+  }
+
+  return self.registration.showNotification(title, options)
+})
--- src/components/firebase-provider/index.js
+++ src/components/firebase-provider/index.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import FirebaseContext from '../firebase-context'
 
+export const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
+
 function FirebaseProvider({ features, children }) {
   const [firebase, setFirebase] = useState(null)
 
@@ -31,16 +42,7 @@ function FirebaseProvider({ features, children }) {
       ]).then(values => {
         const firebaseInstance = values[0]
         if (!firebaseInstance.apps.length) {
-          firebaseInstance.initializeApp({
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            databaseURL: process.env.FIREBASE_DATABASE_URL,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.FIREBASE_APP_ID,
-            measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-          })
+          firebaseInstance.initializeApp(config)
         }
         setFirebase(firebaseInstance)
       })
