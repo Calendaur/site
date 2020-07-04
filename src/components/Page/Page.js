fca857af7842e8dafc776a5238d9c9523a64cb07
Danil Shashkov <shashkovdanil@gmail.com>
2020-07-04 12:39:54 +0500

    Change font

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -179,7 +179,7 @@ function Page({
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
         <link
