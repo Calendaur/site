e004a13b9b5a9a1d0cc98808c3f39c604fc3b47e
Danil Shashkov <shashkovdanil@gmail.com>
2020-07-14 22:41:35 +0500

    Remove unused code

--- src/hooks/index.js
+++
@@ -1,5 +0,0 @@
-import useDidUpdate from './useDidUpdate'
-import useWindowSize from './useWindowSize'
-import useHideOnScroll from './useHideOnScroll'
-
-export { useDidUpdate, useWindowSize, useHideOnScroll }
