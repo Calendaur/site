a4bf768c747c993a94b353b91fc3be3856272f8b
Danil Shashkov <shashkovdanil@gmail.com>
2020-08-13 21:53:26 +0500

    add try/catch

--- public/scripts/push.js
+++ public/scripts/push.js
@@ -1,60 +1,69 @@
 function receivePushNotification(event) {
-  console.log('get push', event, event.data.json())
+  try {
+    console.log('receive push', event, event.data.json())
 
-  const releases = event.data.json()
-  let options
+    const releases = event.data.json()
 
-  if (releases.length === 1) {
-    const [release] = releases
+    let options
 
-    options = {
-      title: release.title,
-      data: `https://released.at/release/${release.id}`,
-      body: release.description,
-      icon: 'https://released.at/images/logo.png',
-      vibrate: [200, 100, 200],
-      tag: 'Released',
-      image: release.cover,
-      badge: 'https://released.at/images/logo.png',
-    }
-  } else {
-    options = {
-      title: `Сегодня вышли: ${releases.map(re => re.title).join(', ')}`,
-      data: `https://released.at/me`,
-      icon: 'https://released.at/images/logo.png',
-      vibrate: [200, 100, 200],
-      tag: 'Released',
-      image: releases[0].cover,
-      badge: 'https://released.at/images/logo.png',
-      actions:
-        releases.length > 4
-          ? [
-              ...releases.slice(0, 3)map(re => ({
+    if (releases.length === 1) {
+      const [release] = releases
+
+      options = {
+        title: release.title,
+        data: `https://released.at/release/${release.id}`,
+        body: release.description,
+        icon: 'https://released.at/images/logo.png',
+        vibrate: [200, 100, 200],
+        tag: 'Released',
+        image: release.cover,
+        badge: 'https://released.at/images/logo.png',
+      }
+    } else {
+      options = {
+        title: `Сегодня вышли: ${releases.map(re => re.title).join(', ')}`,
+        data: `https://released.at/me`,
+        icon: 'https://released.at/images/logo.png',
+        vibrate: [200, 100, 200],
+        tag: 'Released',
+        image: releases[0].cover,
+        badge: 'https://released.at/images/logo.png',
+        actions:
+          releases.length > 4
+            ? [
+                ...releases.slice(0, 3).map(re => ({
+                  action: `https://released.at/release/${re.id}`,
+                  title: re.title,
+                })),
+                { action: `https://released.at/me`, title: 'Смотреть все' },
+              ]
+            : releases.map(re => ({
                 action: `https://released.at/release/${re.id}`,
                 title: re.title,
               })),
-              { action: `https://released.at/me`, title: 'Смотреть все' },
-            ]
-          : releases.map(re => ({
-              action: `https://released.at/release/${re.id}`,
-              title: re.title,
-            })),
+      }
     }
-  }
 
-  const { title, ...rest } = options
-  event.waitUntil(self.registration.showNotification(title, rest))
+    const { title, ...rest } = options
+    event.waitUntil(self.registration.showNotification(title, rest))
+  } catch (e) {
+    console.error('receive error', e)
+  }
 }
 
 function openPushNotification(event) {
-  console.log('open push', event.notification.data)
+  try {
+    console.log('open push', event.notification.data)
 
-  event.notification.close()
+    event.notification.close()
 
-  if (typeof event.action === 'string') {
-    clients.openWindow(event.action)
+    if (typeof event.action === 'string') {
+      clients.openWindow(event.action)
+    }
+  } catch (e) {
+    console.error('open error', e)
   }
 }
 
 self.addEventListener('push', receivePushNotification)
-self.addEventListener('notificationclick', openPushNotification, false)
+self.addEventListener('notificationclick', openPushNotification)
