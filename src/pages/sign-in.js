be0392c34ac8f2bd8f82b9c9357db3d997c87a2e
Danil Shashkov <shashkovdanil@gmail.com>
2020-08-29 22:00:50 +0500

    redirect to me from sign pages if user authed

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
+import { parseCookies } from 'nookies'
 import { AuthForm } from 'components'
+import { redirect } from 'shared/utils'
+import { routes } from 'shared/constants'
+import { me } from 'shared/api'
 
 function SignInPage() {
   return (
@@ -11,6 +15,25 @@ function SignInPage() {
       <AuthForm buttonTitle="Войти" type="login" />
     </>
   )
+}
+
+SignInPage.getInitialProps = async ctx => {
+  try {
+    const { authorization: token } = parseCookies(ctx)
+
+    if (!token) return {}
+
+    const user = await me(token)
+
+    if (user) {
+      redirect(ctx, routes.ME)
+      return {}
+    }
+
+    return {}
+  } catch (e) {
+    return {}
+  }
 }
 
 export default SignInPage
