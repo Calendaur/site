56b2bc7267b4e207eca66c9a97c51209ee5d4c33
Danil Shashkov <shashkovdanil@gmail.com>
2020-10-19 21:31:02 +0500

    return sw

--- next.config.js
+++ next.config.js
@@ -1,40 +1,23 @@
-// const withOffline = require('next-offline')
+const withOffline = require('next-offline')
 
-// const nextConfig = {
-//   workboxOpts: {
-//     importScripts: ['./scripts/push.js'],
-//     swDest: 'static/service-worker.js',
-//     runtimeCaching: [
-//       {
-//         urlPattern: /^https?.*/,
-//         handler: 'NetworkFirst',
-//         options: {
-//           cacheName: 'offlineCache',
-//           networkTimeoutSeconds: 15,
-//           expiration: {
-//             maxEntries: 200,
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/service-worker.js',
-//         destination: '/_next/static/service-worker.js',
-//       },
-//       {
-//         source: '/api/:path*',
-//         destination: 'https://api.released.at/api/:path*',
-//       },
-//     ]
-//   },
-// }
-
-// module.exports = withOffline(nextConfig)
-
-module.exports = {
+const nextConfig = {
+  workboxOpts: {
+    importScripts: ['./scripts/push.js'],
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
@@ -48,3 +31,5 @@ module.exports = {
     ]
   },
 }
+
+module.exports = withOffline(nextConfig)
