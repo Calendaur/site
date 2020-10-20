ac60919a0359ab3e7a6915d87268b27157d7d3a3
Danil Shashkov <shashkovdanil@gmail.com>
2020-10-20 13:23:22 +0500

    remove local fonts and add google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,33 +31,19 @@ class CustomDocument extends Document {
     return (
       <Html lang="ru" prefix="og: https://ogp.me/ns#">
         <Head>
+          <link rel="dns-prefetch" href="//fonts.googleapis.com" />
           <link
-            rel="preload"
-            href="/fonts/Inter/400.woff2"
-            as="font"
-            type="font/woff2"
+            rel="preconnect"
+            href="https://fonts.gstatic.com/"
             crossOrigin="anonymous"
           />
           <link
-            rel="preload"
-            href="/fonts/Inter/600.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&family=Merriweather:ital,wght@0,400;0,700;1,400&display=swap"
+            rel="stylesheet"
           />
           <link
-            rel="preload"
-            href="/fonts/Inter/800.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/Montserrat/200.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&text=released&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>
