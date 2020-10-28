73f6b18ab583334ecfc3cc6f86501bdafc7d7a12
Danil Shashkov <shashkovdanil@gmail.com>
2020-10-28 10:38:39 +0500

    remove next-offline

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
-const withOffline = require('next-offline')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 const nextConfig = {
-  workboxOpts: {
-    importScripts: ['./scripts/push.js'],
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    runtimeCaching,
   },
   async rewrites() {
     return [
@@ -35,4 +24,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withOffline(nextConfig)
+module.exports = withPWA(nextConfig)
