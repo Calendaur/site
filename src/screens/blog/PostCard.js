852e0623bb808e6cac6c77a60acebf87748035bb
Danil Shashkov <shashkovdanil@gmail.com>
2020-10-31 13:18:55 +0500

    fix

--- src/screens/blog/PostCard.js
+++ src/screens/blog/PostCard.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import format from 'date-fns/format'
-import ru from 'date-fns/locale/ru'
 import slugify from '@sindresorhus/slugify'
 import { A } from 'components'
 import { routes } from 'shared/constants'
 
-function PostCard({ title, id, updated_at }) {
+function PostCard({ title, id, created_at }) {
   const slug = slugify(title)
-  const date = new Date(updated_at.split(' ')[0])
 
   return (
     <A href="/blog/[id]" as={routes.BLOG_POST(`${id}-${slug}`)}>
       <h2>{title}</h2>
-      <p>{format(date, 'd MMMM', { locale: ru })}</p>
     </A>
   )
 }
